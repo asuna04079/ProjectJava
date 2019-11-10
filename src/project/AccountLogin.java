d41cb913368c24508ae778c0dcdfb1386e65d78b
asuna04079 <48233991+asuna04079@users.noreply.github.com>
2019-11-10 19:22:44 +0700

    Add files via upload

---
+++ src/project/AccountLogin.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+/**
+ *
+ * @author POOM
+ */
+public class AccountLogin {
+    private static int id;
+    public void setId(int n){
+        id = n;
+    }
+    public int getId(){
+        return id;
+    }
+    
+}
